4aca3c12e74d11f90472778a7c48bc0357422113
RaphaelDD885 <56082307+RaphaelDD885@users.noreply.github.com>
2022-11-10 10:44:10 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
+let x = 0
 basic.forever(function () {
+    x = randint(0, 4)
     for (let Index = 0; Index <= 4; Index++) {
-        led.toggle(Index, Index)
+        led.toggle(x, Index)
         basic.pause(100)
-        led.toggle(Index, Index)
+        led.toggle(x, Index)
     }
+    basic.pause(100)
 })
